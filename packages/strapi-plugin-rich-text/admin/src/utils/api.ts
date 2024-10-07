81097d6f6d276072bd8d77ab59a4a98f9cf73926
Luis Alaguna <alagunasalahaddin@live.com>
2024-10-07 16:32:07 -0500

    chore: implement internal links on admin

--- packages/strapi-plugin-rich-text/admin/src/utils/api.ts
+++ packages/strapi-plugin-rich-text/admin/src/utils/api.ts
@@ -2,8 +2,17 @@ import { request } from "@strapi/helper-plugin";
 
 import pluginPkg from "../../../package.json";
 import type { Settings } from "../../../types/settings";
+import { ContentType } from "../types";
 
 const name = pluginPkg.strapi.name;
+
+export function getContentTypes(): Array<ContentType> {
+  return request(`/${name}/content-types`, { method: "GET" });
+}
+
+export function getEntries(id: string) {
+  return request(`/${name}/content-types/${id}`, { method: "GET" });
+}
 
 export function getSettings(): Settings {
   return request(`/${name}/settings`, { method: "GET" });
